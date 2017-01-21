0f9ca20c8c24b35f39f23ec5256582657261877c
Dustin Spicuzza <dustin@virtualroadside.com>
2017-01-21 02:11:16 -0500

    Add opencv example for non-linux platforms

---
+++ examples/cvstream.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# WARNING: You should only use this approach for testing cscore on platforms that
+#          it doesn't support using UsbCamera (Windows or OSX).
+#
+
+import cscore as cs
+
+if hasattr(cs, 'UsbCamera'):
+    camera = cs.UsbCamera("usbcam", 0)
+    camera.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
+else:
+    import cv2
+    import threading
+    
+    camera = cs.CvSource("cvsource", cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
+    
+    # tell OpenCV to capture video for us
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    
+    # Do it in another thread
+    def _thread():
+        img = None
+        while True:
+            retval, img = cap.read(img)
+            if retval:
+                camera.putFrame(img)
+        
+    th = threading.Thread(target=_thread, daemon=True)
+    th.start()
+    
+
+mjpegServer = cs.MjpegServer("httpserver", 8081)
+mjpegServer.setSource(camera);
+
+print("mjpg server listening at http://0.0.0.0:8081")
+input("Press enter to exit...")
