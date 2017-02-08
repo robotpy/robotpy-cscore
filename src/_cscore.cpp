c130d6ef5d7905a3a9930594b06c35a74625b8ca
Dustin Spicuzza <dustin@virtualroadside.com>
2017-02-08 02:43:16 -0500

    Even more things work now

--- src/_cscore.cpp
+++ src/_cscore.cpp
@@ -3,6 +3,7 @@
 #include <pybind11/functional.h>
 #include <pybind11/operators.h>
 #include <pybind11/stl.h>
+#include "release_gil.hpp"
 namespace py = pybind11;
 
 #include "cscore_oo.h"
@@ -23,6 +24,8 @@ PYBIND11_PLUGIN(_cscore) {
     static int unused; // the capsule needs something to reference
     py::capsule cleanup(&unused, [](PyObject *) { CS_Destroy(); });
     m.add_object("_cleanup", cleanup);
+    
+    m.def("breakpoint", []() -> void { __asm__ volatile("int $0x03"); });
     
     // cscore_cpp.h
     
@@ -57,7 +60,7 @@ PYBIND11_PLUGIN(_cscore) {
       .value("kBGR", VideoMode::PixelFormat::kBGR)
       .value("kGray", VideoMode::PixelFormat::kGray);
     
-    #define def_status_fn(n, N, T) def(n, [](T t) { CS_Status s = 0; return N(t, &s); })
+    #define def_status_fn(n, N, T) def(n, [](T t) { py::gil_scoped_release __r; CS_Status s = 0; return N(t, &s); })
     
     m.def("getNetworkInterfaces", &GetNetworkInterfaces)
      .def_status_fn("getHttpCameraUrls", GetHttpCameraUrls, CS_Source)
@@ -71,33 +74,27 @@ PYBIND11_PLUGIN(_cscore) {
     py::class_<VideoProperty> videoproperty(m, "VideoProperty");
     videoproperty
       .def(py::init<>())
-      .def("getName", &VideoProperty::GetName)
-      .def("getKind", &VideoProperty::GetKind)
-      .def("isBoolean", &VideoProperty::IsBoolean)
-      .def("isInteger", &VideoProperty::IsInteger)
-      .def("isString", &VideoProperty::IsString)
-      .def("isEnum", &VideoProperty::IsEnum)
-      .def("get", &VideoProperty::Get)
-      .def("set", [](VideoProperty &__inst, int value) {
-        py::gil_scoped_release __release;
-        __inst.Set(value);
-      })
-      .def("getMin", &VideoProperty::GetMin)
-      .def("getMax", &VideoProperty::GetMax)
-      .def("getStep", &VideoProperty::GetStep)
-      .def("getDefault", &VideoProperty::GetDefault)
-      .def("getString", (std::string (VideoProperty::*)() const)&VideoProperty::GetString)
+      .def("getName", py::release_gil(&VideoProperty::GetName))
+      .def("getKind", py::release_gil(&VideoProperty::GetKind))
+      .def("isBoolean", py::release_gil(&VideoProperty::IsBoolean))
+      .def("isInteger", py::release_gil(&VideoProperty::IsInteger))
+      .def("isString", py::release_gil(&VideoProperty::IsString))
+      .def("isEnum", py::release_gil(&VideoProperty::IsEnum))
+      .def("get", py::release_gil(&VideoProperty::Get))
+      .def("set", py::release_gil(&VideoProperty::Set))
+      .def("getMin", py::release_gil(&VideoProperty::GetMin))
+      .def("getMax", py::release_gil(&VideoProperty::GetMax))
+      .def("getStep", py::release_gil(&VideoProperty::GetStep))
+      .def("getDefault", py::release_gil(&VideoProperty::GetDefault))
+      .def("getString", py::release_gil((std::string (VideoProperty::*)() const)&VideoProperty::GetString))
       /*.def("GetString", [](VideoProperty &__inst) {
         llvm::SmallVectorImpl<char> buf;
         auto __ret = __inst.GetString(buf);
         return std::make_tuple(__ret, buf);
       })*/
-      .def("setString", [](VideoProperty &__inst, llvm::StringRef value) {
-        py::gil_scoped_release __release;
-        __inst.SetString(value);
-      })
-      .def("getChoices", &VideoProperty::GetChoices)
-      .def("getLastStatus", &VideoProperty::GetLastStatus);
+      .def("setString", py::release_gil(&VideoProperty::SetString))
+      .def("getChoices", py::release_gil(&VideoProperty::GetChoices))
+      .def("getLastStatus", py::release_gil(&VideoProperty::GetLastStatus));
     
     py::enum_<VideoProperty::Kind>(videoproperty, "Kind")
       .value("kNone", VideoProperty::Kind::kNone)
@@ -119,34 +116,24 @@ PYBIND11_PLUGIN(_cscore) {
       .def("getDescription", &VideoSource::GetDescription)
       .def("getLastFrameTime", &VideoSource::GetLastFrameTime)
       .def("isConnected", &VideoSource::IsConnected)
-      .def("getProperty", &VideoSource::GetProperty)
-      .def("enumerateProperties", &VideoSource::EnumerateProperties)
-      .def("getVideoMode", &VideoSource::GetVideoMode)
-      .def("setVideoMode", [](VideoSource &__inst) {
-        VideoMode mode; py::gil_scoped_release __release;
-        auto __ret = __inst.SetVideoMode(mode);
-        return std::make_tuple(__ret, mode);
+      .def("getProperty", py::release_gil(&VideoSource::GetProperty))
+      .def("enumerateProperties", py::release_gil(&VideoSource::EnumerateProperties))
+      .def("getVideoMode", py::release_gil(&VideoSource::GetVideoMode))
+      .def("setVideoMode", [](VideoSource &__inst, VideoMode mode) {
+        py::gil_scoped_release __release;
+        return __inst.SetVideoMode(mode);
       })
       .def("setVideoMode", [](VideoSource &__inst, VideoMode::PixelFormat pixelFormat, int width, int height, int fps) {
         py::gil_scoped_release __release;
         return __inst.SetVideoMode(pixelFormat, width, height, fps);
       })
-      .def("setPixelFormat", [](VideoSource &__inst, VideoMode::PixelFormat pixelFormat) {
-        py::gil_scoped_release __release;
-        return __inst.SetPixelFormat(pixelFormat);
-      })
-      .def("setResolution", [](VideoSource &__inst, int width, int height) {
-        py::gil_scoped_release __release;
-        return __inst.SetResolution(width, height);
-      })
-      .def("setFPS", [](VideoSource &__inst, int fps) {
-        py::gil_scoped_release __release;
-        return __inst.SetFPS(fps);
-      })
-      .def("enumerateVideoModes", &VideoSource::EnumerateVideoModes)
-      .def("getLastStatus", &VideoSource::GetLastStatus)
-      .def("enumerateSinks", &VideoSource::EnumerateSinks)
-      .def_static("enumerateSources", &VideoSource::EnumerateSources);
+      .def("setPixelFormat", py::release_gil(&VideoSource::SetPixelFormat))
+      .def("setResolution", py::release_gil(&VideoSource::SetResolution))
+      .def("setFPS", py::release_gil(&VideoSource::SetFPS))
+      .def("enumerateVideoModes", py::release_gil(&VideoSource::EnumerateVideoModes))
+      .def("getLastStatus", py::release_gil(&VideoSource::GetLastStatus))
+      .def("enumerateSinks", py::release_gil(&VideoSource::EnumerateSinks))
+      .def_static("enumerateSources", py::release_gil(&VideoSource::EnumerateSources));
       //.def("__repr__", [](VideoSource &__inst){
       //  std::stringstream id;
       //  id << (void const *)&__inst;
@@ -162,35 +149,14 @@ PYBIND11_PLUGIN(_cscore) {
     py::class_<VideoCamera, VideoSource> videocamera(m, "VideoCamera");
     videocamera
       .def(py::init<>())
-      .def("setBrightness", [](VideoCamera &__inst, int brightness) {
-        py::gil_scoped_release __release;
-        __inst.SetBrightness(brightness);
-      })
-      .def("getBrightness", &VideoCamera::GetBrightness)
-      .def("setWhiteBalanceAuto", [](VideoCamera &__inst) {
-        py::gil_scoped_release __release;
-        __inst.SetWhiteBalanceAuto();
-      })
-      .def("setWhiteBalanceHoldCurrent", [](VideoCamera &__inst) {
-        py::gil_scoped_release __release;
-        __inst.SetWhiteBalanceHoldCurrent();
-      })
-      .def("setWhiteBalanceManual", [](VideoCamera &__inst, int value) {
-        py::gil_scoped_release __release;
-        __inst.SetWhiteBalanceManual(value);
-      })
-      .def("setExposureAuto", [](VideoCamera &__inst) {
-        py::gil_scoped_release __release;
-        __inst.SetExposureAuto();
-      })
-      .def("setExposureHoldCurrent", [](VideoCamera &__inst) {
-        py::gil_scoped_release __release;
-        __inst.SetExposureHoldCurrent();
-      })
-      .def("setExposureManual", [](VideoCamera &__inst, int value) {
-        py::gil_scoped_release __release;
-        __inst.SetExposureManual(value);
-      });
+      .def("setBrightness", py::release_gil(&VideoCamera::SetBrightness))
+      .def("getBrightness", py::release_gil(&VideoCamera::GetBrightness))
+      .def("setWhiteBalanceAuto", py::release_gil(&VideoCamera::SetWhiteBalanceAuto))
+      .def("setWhiteBalanceHoldCurrent", py::release_gil(&VideoCamera::SetWhiteBalanceHoldCurrent))
+      .def("setWhiteBalanceManual", py::release_gil(&VideoCamera::SetWhiteBalanceManual))
+      .def("setExposureAuto", py::release_gil(&VideoCamera::SetExposureAuto))
+      .def("setExposureHoldCurrent", py::release_gil(&VideoCamera::SetExposureHoldCurrent))
+      .def("setExposureManual", py::release_gil(&VideoCamera::SetExposureManual));
     
     py::enum_<VideoCamera::WhiteBalance>(videocamera, "WhiteBalance")
       .value("kFixedIndoor", VideoCamera::WhiteBalance::kFixedIndoor)
@@ -205,7 +171,7 @@ PYBIND11_PLUGIN(_cscore) {
       .def(py::init<>())
       .def(py::init<llvm::StringRef,int>())
       .def(py::init<llvm::StringRef,llvm::StringRef>())
-      .def_static("enumerateUsbCameras", &UsbCamera::EnumerateUsbCameras)
+      .def_static("enumerateUsbCameras", py::release_gil(&UsbCamera::EnumerateUsbCameras))
       .def("getPath", &UsbCamera::GetPath);
 #endif
 
@@ -226,10 +192,10 @@ PYBIND11_PLUGIN(_cscore) {
       .def(py::init<llvm::StringRef,llvm::ArrayRef<std::string>,HttpCamera::HttpCameraKind>(),
            py::arg("name"), py::arg("urls"), py::arg("kind") = HttpCamera::HttpCameraKind::kUnknown)
       //.def(py::init<llvm::StringRef,std::initializer_list<T>,cs::HttpCamera::HttpCameraKind>())
-      .def("getHttpCameraKind", &HttpCamera::GetHttpCameraKind)
-      .def("setUrls", (void (HttpCamera::*)(llvm::ArrayRef<std::string>))&HttpCamera::SetUrls)
+      .def("getHttpCameraKind", py::release_gil(&HttpCamera::GetHttpCameraKind))
+      .def("setUrls", py::release_gil((void (HttpCamera::*)(llvm::ArrayRef<std::string>))&HttpCamera::SetUrls))
       //.def("SetUrls", (void (HttpCamera::*)(std::initializer_list<T>))&HttpCamera::SetUrls)
-      .def("getUrls", &HttpCamera::GetUrls);
+      .def("getUrls", py::release_gil(&HttpCamera::GetUrls));
     
     py::class_<AxisCamera, HttpCamera> axiscamera(m, "AxisCamera");
     axiscamera
@@ -251,12 +217,8 @@ PYBIND11_PLUGIN(_cscore) {
       .def("notifyError", &CvSource::NotifyError)
       .def("setConnected", &CvSource::SetConnected)
       .def("setDescription", &CvSource::SetDescription)
-      .def("createProperty", &CvSource::CreateProperty)
-      .def("setEnumPropertyChoices", [](CvSource &__inst, llvm::ArrayRef<std::string> choices) {
-        cs::VideoProperty property;
-        __inst.SetEnumPropertyChoices(property, choices);
-        return property;
-    });
+      .def("createProperty", py::release_gil(&CvSource::CreateProperty))
+      .def("setEnumPropertyChoices", py::release_gil((void (CvSource::*)(const VideoProperty &property, llvm::ArrayRef<std::string> choices))&CvSource::SetEnumPropertyChoices));
       /*.def("SetEnumPropertyChoices", [](CvSource &__inst, std::initializer_list<T> choices) {
         cs::VideoProperty property;
         auto __ret = __inst.SetEnumPropertyChoices(property, choices);
@@ -274,14 +236,11 @@ PYBIND11_PLUGIN(_cscore) {
       .def("getKind", &VideoSink::GetKind)
       .def("getName", &VideoSink::GetName)
       .def("getDescription", &VideoSink::GetDescription)
-      .def("setSource", [](VideoSink &__inst, VideoSource source) {
-        py::gil_scoped_release __release;
-        __inst.SetSource(source);
-      })
-      .def("getSource", &VideoSink::GetSource)
-      .def("getSourceProperty", &VideoSink::GetSourceProperty)
+      .def("setSource", py::release_gil(&VideoSink::SetSource))
+      .def("getSource", py::release_gil(&VideoSink::GetSource))
+      .def("getSourceProperty", py::release_gil(&VideoSink::GetSourceProperty))
       .def("getLastStatus", &VideoSink::GetLastStatus)
-      .def_static("enumerateSinks", &VideoSink::EnumerateSinks);
+      .def_static("enumerateSinks", py::release_gil(&VideoSink::EnumerateSinks));
     
     py::enum_<VideoSink::Kind>(videosink, "Kind")
       .value("kUnknown", VideoSink::Kind::kUnknown)
